79efd8a472da1527315ccc65baf81bcc8e3738a2
Eric Mlynar <emlynar@users.noreply.github.com>
2018-07-21 16:51:41 -0600

    Dynamic HTML Start
    I added HTML via JavaScript according to what data was hardcoded in. Just a start.

--- Budget/app.js
+++ Budget/app.js
@@ -71,4 +71,70 @@ class BudgetItem {
   }, 2000);
 
   setTimeout(() => draw(), 2250);
+
+  // Trial: Creating Dynamic HTML
+  setTimeout(() => {
+    var tree = document.createDocumentFragment();
+    var itemlist = activeBudget.getItems();
+
+    // Row 1 setup
+    var div1 = document.createElement("div");
+    div1.setAttribute("class", "row");
+
+    var div1col1 = document.createElement("div");
+    div1col1.setAttribute("class", "col");
+    div1col1.innerHTML = itemlist[0].id;
+    var div1col2 = document.createElement("div");
+    div1col2.setAttribute("class", "col-6");
+    div1col2.innerHTML = itemlist[0].description;
+    var div1col3 = document.createElement("div");
+    div1col3.setAttribute("class", "col");
+    div1col3.innerHTML = itemlist[0].value;
+
+    div1.appendChild(div1col1);
+    div1.appendChild(div1col2);
+    div1.appendChild(div1col3);
+
+    // Row 2 setup
+    var div2 = document.createElement("div");
+    div2.setAttribute("class", "row");
+
+    var div2col1 = document.createElement("div");
+    div2col1.setAttribute("class", "col");
+    div2col1.innerHTML = itemlist[1].id;
+    var div2col2 = document.createElement("div");
+    div2col2.setAttribute("class", "col-6");
+    div2col2.innerHTML = itemlist[1].description;
+    var div2col3 = document.createElement("div");
+    div2col3.setAttribute("class", "col");
+    div2col3.innerHTML = itemlist[1].value;
+
+    div2.appendChild(div2col1);
+    div2.appendChild(div2col2);
+    div2.appendChild(div2col3);
+
+    // Row 3 setup
+    var div3 = document.createElement("div");
+    div3.setAttribute("class", "row");
+
+    var div3col1 = document.createElement("div");
+    div3col1.setAttribute("class", "col");
+    div3col1.innerHTML = itemlist[2].id;
+    var div3col2 = document.createElement("div");
+    div3col2.setAttribute("class", "col-6");
+    div3col2.innerHTML = itemlist[2].description;
+    var div3col3 = document.createElement("div");
+    div3col3.setAttribute("class", "col");
+    div3col3.innerHTML = itemlist[2].value;
+
+    div3.appendChild(div3col1);
+    div3.appendChild(div3col2);
+    div3.appendChild(div3col3);
+
+    // Add all 3 rows to main div
+    tree.appendChild(div1);
+    tree.appendChild(div2);
+    tree.appendChild(div3);
+    document.getElementById("main").appendChild(tree);
+    }, 5000);
 })();
