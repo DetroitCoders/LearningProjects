58bf197ecfbf355d6be5f8d9bf36585ba3ba8211
Eric Mlynar <emlynar@users.noreply.github.com>
2019-06-01 17:40:09 -0600

    Added disfunctional delete button

--- TipCalc/app.js
+++ TipCalc/app.js
@@ -6,6 +6,7 @@ function calcTip() {
     var tip = billAmount * service;
     var totalBill = tip + billAmount;
     var totalBillPerPerson = totalBill / numOfPeople;
+    var id = guid();
 
     var ansBlock1 = document.createElement('p');
     ansBlock1.innerText = "Your tip total is: " + tip;
@@ -16,12 +17,42 @@ function calcTip() {
     var ansBlock3 = document.createElement('p');
     ansBlock3.innerText = "Your bill per person is: " + totalBillPerPerson;
 
+    var deleteButton = document.createElement('button');
+    deleteButton.setAttribute("type", "button");
+    deleteButton.setAttribute("class", "btn btn-danger");
+    var deleteId = "deleteTip(" + id + ")";
+    deleteButton.setAttribute("onclick", deleteId);
+    deleteButton.innerText = "Delete";
+
     var ansDiv = document.createElement('div');
     ansDiv.setAttribute("class", "container bg-dark text-white");
+    ansDiv.setAttribute("id", id);
+    var horzDivide = document.createElement('hr');
 
     ansDiv.appendChild(ansBlock1);
     ansDiv.appendChild(ansBlock2);
     ansDiv.appendChild(ansBlock3);
+    ansDiv.appendChild(deleteButton);
+    ansDiv.appendChild(horzDivide);
 
     document.getElementById('tipCalcContainer').appendChild(ansDiv);
 }
+
+function deleteTip(id) {
+    var child = document.getElementById(id);
+    child.parentNode.removeChild(child);
+}
+
+function guid() {
+    return (
+      s4() +
+      s4() +
+      s4()
+    );
+  }
+
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
