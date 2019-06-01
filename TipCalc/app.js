ea5ce1358413863aff719157a16a99cd0e742d35
Eric Mlynar <emlynar@users.noreply.github.com>
2019-06-01 16:04:00 -0600

    Working Calculations.

--- TipCalc/app.js
+++ TipCalc/app.js
@@ -1,9 +1,27 @@
 function calcTip() {
-    var billAmount = document.getElementById('inputBillAmount').value;
-    var service = document.getElementById('inputService').value;
+    var billAmount = parseFloat(document.getElementById('inputBillAmount').value);
+    var service = parseFloat(document.getElementById('inputService').value);
+    var numOfPeople = parseFloat(document.getElementById('numOfPeople').value);
 
-    var answer = billAmount * service;
-    var ansBlock = document.createElement('p');
-    ansBlock.innerText = "Your tip total is: " + answer;
-    document.getElementById('answerBlock').appendChild(ansBlock);
+    var tip = billAmount * service;
+    var totalBill = tip + billAmount;
+    var totalBillPerPerson = totalBill / numOfPeople;
+
+    var ansBlock1 = document.createElement('p');
+    ansBlock1.innerText = "Your tip total is: " + tip;
+
+    var ansBlock2 = document.createElement('p');
+    ansBlock2.innerText = "Your total amount is: " + totalBill;
+
+    var ansBlock3 = document.createElement('p');
+    ansBlock3.innerText = "Your bill per person is: " + totalBillPerPerson;
+
+    var ansDiv = document.createElement('div');
+    ansDiv.setAttribute("class", "container bg-dark text-white");
+
+    ansDiv.appendChild(ansBlock1);
+    ansDiv.appendChild(ansBlock2);
+    ansDiv.appendChild(ansBlock3);
+
+    document.getElementById('tipCalcContainer').appendChild(ansDiv);
 }
