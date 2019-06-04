b33dc696d85ea5ea1563042879f417ee6f6a6cfe
Eric Mlynar <emlynar@users.noreply.github.com>
2019-06-04 11:10:44 -0600

    Validation complete for user end and back end.

--- TipCalc/app.js
+++ TipCalc/app.js
@@ -3,44 +3,57 @@ function calcTip() {
     var service = parseFloat(document.getElementById('inputService').value);
     var numOfPeople = parseFloat(document.getElementById('numOfPeople').value);
 
-    var tip = billAmount * service;
-    var totalBill = tip + billAmount;
-    var totalBillPerPerson = totalBill / numOfPeople;
-    var id = guid();
+    var isValid = validate(billAmount, service, numOfPeople);
+    if(isValid) {
+      var tip = billAmount * service;
+      var totalBill = tip + billAmount;
+      var totalBillPerPerson = totalBill / numOfPeople;
+      var id = guid();
 
-    var ansBlock1 = document.createElement('p');
-    ansBlock1.innerText = "Your tip total is: " + tip;
+      var ansBlock1 = document.createElement('p');
+      ansBlock1.innerText = "Your tip total is: " + tip;
 
-    var ansBlock2 = document.createElement('p');
-    ansBlock2.innerText = "Your total amount is: " + totalBill;
+      var ansBlock2 = document.createElement('p');
+      ansBlock2.innerText = "Your total amount is: " + totalBill;
 
-    var ansBlock3 = document.createElement('p');
-    ansBlock3.innerText = "Your bill per person is: " + totalBillPerPerson;
+      var ansBlock3 = document.createElement('p');
+      ansBlock3.innerText = "Your bill per person is: " + totalBillPerPerson;
 
-    var deleteButton = document.createElement('button');
-    deleteButton.setAttribute("type", "button");
-    deleteButton.setAttribute("class", "btn btn-danger");
-    var deleteId = "deleteTip(\'" + id + "\')";
-    deleteButton.setAttribute("onclick", deleteId);
-    deleteButton.innerText = "Delete";
+      var deleteButton = document.createElement('button');
+      deleteButton.setAttribute("type", "button");
+      deleteButton.setAttribute("class", "btn btn-danger");
+      var deleteId = "deleteTip(\'" + id + "\')";
+      deleteButton.setAttribute("onclick", deleteId);
+      deleteButton.innerText = "Delete";
 
-    var ansDiv = document.createElement('div');
-    ansDiv.setAttribute("class", "container bg-dark text-white");
-    ansDiv.setAttribute("id", id);
-    var horzDivide = document.createElement('hr');
+      var ansDiv = document.createElement('div');
+      ansDiv.setAttribute("class", "container bg-dark text-white");
+      ansDiv.setAttribute("id", id);
+      var horzDivide = document.createElement('hr');
 
-    ansDiv.appendChild(ansBlock1);
-    ansDiv.appendChild(ansBlock2);
-    ansDiv.appendChild(ansBlock3);
-    ansDiv.appendChild(deleteButton);
-    ansDiv.appendChild(horzDivide);
+      ansDiv.appendChild(ansBlock1);
+      ansDiv.appendChild(ansBlock2);
+      ansDiv.appendChild(ansBlock3);
+      ansDiv.appendChild(deleteButton);
+      ansDiv.appendChild(horzDivide);
 
-    document.getElementById('tipCalcContainer').appendChild(ansDiv);
+      document.getElementById('tipCalcContainer').appendChild(ansDiv);
+    }
+}
+
+function validate(billAmount, service, numOfPeople) {
+  var isTrue = false;
+
+  if((!isNaN(billAmount)) && (!isNaN(service)) && (!isNaN(numOfPeople))) {
+    isTrue = true;
+  }
+
+  return isTrue;
 }
 
 function deleteTip(id) {
-    var child = document.getElementById(id);
-    child.parentNode.removeChild(child);
+  var child = document.getElementById(id);
+  child.parentNode.removeChild(child);
 }
 
 function guid() {
