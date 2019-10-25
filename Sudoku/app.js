0a959649f3acb600a174274500e65fe9efba8b86
Eric Mlynar <mlynare@gmail.com>
2019-10-25 12:10:18 -0600

    Quad Logic Part 1

--- Sudoku/app.js
+++ Sudoku/app.js
@@ -10,13 +10,14 @@ function solveBoard() {
         [9, 6, 8, 2, 3, 1, 5, 7, 4],
         [5, 7, 4, 9, 6, 8, 2, 3, 1]
     ];
-
-    for (let row = 0, fd = 1; row < 9; row++, fd++) {
+    
+    // Populate Board with JavaScript gameBoard.
+    /*for (let row = 0, fd = 1; row < 9; row++, fd++) {
         for (let col = 0, sd = 1; col < 9; col++, sd++) {
             let placement = fd.toString() + sd.toString();
             document.getElementById(placement).value = gameBoard[row][col];
         }
-    }
+    } */
 
     function checkRow(rowNumber) {
         var rowUnique = true;
@@ -101,3 +102,47 @@ function checkNumMatch(x, y) {
     }
     return false;
 }
+
+function findQuads() {
+    var gameBoard = [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [7, 8, 9, 1, 2, 3, 4, 5, 6],
+      [4, 5, 6, 7, 8, 9, 1, 2, 3],
+      [3, 1, 2, 8, 4, 5, 9, 6, 7],
+      [6, 9, 7, 3, 1, 2, 8, 4, 5],
+      [8, 4, 5, 6, 9, 7, 3, 1, 2],
+      [2, 3, 1, 5, 7, 4, 6, 9, 8],
+      [9, 6, 8, 2, 3, 1, 5, 7, 4],
+      [5, 7, 4, 9, 6, 8, 2, 3, 1]
+    ];
+  
+    var flattenedQuads = [];
+  
+    for (var q = 0; q < 9; q++) {
+      var quadCords = getQuadCords(q);
+      var quad = [];
+  
+      var x = quadCords[1];
+      for (var currentQuadRowIdx = x; currentQuadRowIdx < x + 3; currentQuadRowIdx++) {
+        var y = quadCords[0];
+        for (var currentQuadColumnIdx = y; currentQuadColumnIdx < y + 3; currentQuadColumnIdx++) {
+          quad.push(gameBoard[currentQuadRowIdx][currentQuadColumnIdx]);
+        }
+      }
+      // Check Quad
+      flattenedQuads.push(quad);
+    }
+  
+    console.table(flattenedQuads);
+  }
+  
+  function getQuadCords(quadIdx) {
+    var quotient = quadIdx / 3;
+    var row = Math.floor(quotient) * 3;
+    var column = Math.floor((quotient % 1) / 0.33) * 3;
+    return [column, row];
+  }
+  
+  /** 7 bottom,middel
+   * column 7/3 = 2.333 mod 1 = .333 / .33 == 1.01010101 floor incase extra decimal places
+   * */
